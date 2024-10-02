7fd49ff09d59751b21fd9d27ec35cac17e6b0692
pablushaa <glusha189@gmail.com>
2024-10-02 17:42:03 +0300

    1.2 is very very soon...

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -10,13 +10,13 @@
 
 #define OLED_RESET -1
 
-const char* ssid = ""; // !!! SET UP THIS BEFORE COMPILING !!!
-const char* pass = "";
+const char* ssid = "ALEX"; // !!! SET UP THIS BEFORE COMPILING !!!
+const char* pass = "magadan73";
 
 String ip = "";
-
+uint16_t curColor = SSD1306_WHITE;
+bool inverted = false;
 WebServer server(80);
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void index();
@@ -26,7 +26,18 @@ void print();
 void setCursor();
 void setSize();
 void update();
+void setColor();
 void drawPixel();
+void drawLine();
+void drawRect();
+void drawFilledRect();
+void drawRoundRect();
+void drawFilledRoundRect();
+void drawCircle();
+void drawFilledCircle();
+void drawTriangle();
+void drawFilledTriangle();
+void invert();
 
 void setup()
 {
@@ -71,7 +82,17 @@ void setup()
   server.on("/setCursor", setCursor);
   server.on("/setSize", setSize);
   server.on("/update", update);
+  server.on("/setColor", setColor);
   server.on("/drawPixel", drawPixel);
+  server.on("/drawLine", drawLine);
+  server.on("/drawRect", drawRect);
+  server.on("/drawFilledRect", drawFilledRect);
+  server.on("/drawRoundRect", drawRoundRect);
+  server.on("/drawFilledRoundRect", drawFilledRoundRect);
+  server.on("/drawCircle", drawCircle);
+  server.on("/drawFilledCircle", drawFilledCircle);
+  server.on("/invert", invert);
+  server.on("/drawTriangle", drawTriangle);
   server.on("/", ind);
   server.begin();
 }
@@ -97,6 +118,7 @@ void println() {
   if (server.method() == HTTP_POST) {
     if (server.hasArg("text")) {
       String text = server.arg("text");
+
       display.println(text);
       server.send(200, "text/plain", "OK");
     } else {
@@ -111,6 +133,7 @@ void print() {
   if (server.method() == HTTP_POST) {
     if (server.hasArg("text")) {
       String text = server.arg("text");
+
       display.print(text);
       server.send(200, "text/plain", "OK");
     } else {
@@ -127,6 +150,7 @@ void setCursor() {
       int x, y;
       x = server.arg("x").toInt();
       y = server.arg("y").toInt();
+
       display.setCursor(x, y);
       server.send(200, "text/plain", "OK");
     } else {
@@ -141,6 +165,7 @@ void setSize() {
   if (server.method() == HTTP_POST) {
     if (server.hasArg("size")) {
       int size = server.arg("size").toInt();
+
       display.setTextSize(size);
       server.send(200, "text/plain", "OK");
     } else {
@@ -160,13 +185,221 @@ void update() {
   }
 }
 
+void invert() {
+  if (server.method() == HTTP_POST) {
+    inverted = !inverted;
+    display.invertDisplay(inverted);
+    server.send(200, "text/plain", "OK");
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void setColor() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("color")) {
+      String color = server.arg("color");
+      color.toLowerCase();
+      if (color == "white") {
+        display.setTextColor(SSD1306_WHITE);
+        curColor = SSD1306_WHITE;
+        server.send(200, "text/plain", "OK");
+      } else if (color == "black") {
+        display.setTextColor(SSD1306_BLACK);
+        curColor = SSD1306_BLACK;
+        server.send(200, "text/plain", "OK");
+      }
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
 void drawPixel() {
   if (server.method() == HTTP_POST) {
     if (server.hasArg("x") && server.hasArg("y")) {
       int x, y;
       x = server.arg("x").toInt();
       y = server.arg("y").toInt();
+      
       display.drawPixel(x, y, SSD1306_WHITE);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void drawLine() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("x0") && server.hasArg("y0") && server.hasArg("x1") && server.hasArg("y1")) {
+      int x0, y0, x1, y1;
+      x0 = server.arg("x0").toInt();
+      y0 = server.arg("y0").toInt();
+      x1 = server.arg("x1").toInt();
+      y1 = server.arg("y1").toInt();
+      
+      display.drawLine(x0, y0, x1, y1, curColor);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void drawRect() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("x") && server.hasArg("y") && server.hasArg("width") && server.hasArg("height")) {
+      int x, y, width, height;
+      x = server.arg("x").toInt();
+      y = server.arg("y").toInt();
+      width = server.arg("width").toInt();
+      height = server.arg("height").toInt();
+      
+      display.drawRect(x, y, width, height, curColor);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void drawFilledRect() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("x") && server.hasArg("y") && server.hasArg("width") && server.hasArg("height")) {
+      int x, y, width, height;
+      x = server.arg("x").toInt();
+      y = server.arg("y").toInt();
+      width = server.arg("width").toInt();
+      height = server.arg("height").toInt();
+      
+      display.fillRect(x, y, width, height, curColor);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void drawRoundRect() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("x") && server.hasArg("y") && server.hasArg("width") && server.hasArg("height") && server.hasArg("radius")) {
+      int x, y, width, height, radius;
+      x = server.arg("x").toInt();
+      y = server.arg("y").toInt();
+      width = server.arg("width").toInt();
+      height = server.arg("height").toInt();
+      radius = server.arg("radius").toInt();
+      
+      display.drawRoundRect(x, y, width, height, radius, curColor);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void drawFilledRoundRect() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("x") && server.hasArg("y") && server.hasArg("width") && server.hasArg("height") && server.hasArg("radius")) {
+      int x, y, width, height, radius;
+      x = server.arg("x").toInt();
+      y = server.arg("y").toInt();
+      width = server.arg("width").toInt();
+      height = server.arg("height").toInt();
+      radius = server.arg("radius").toInt();
+      
+      display.fillRoundRect(x, y, width, height, radius, curColor);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void drawCircle() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("x") && server.hasArg("y") && server.hasArg("radius")) {
+      int x, y, radius;
+      x = server.arg("x").toInt();
+      y = server.arg("y").toInt();
+      radius = server.arg("radius").toInt();
+      
+      display.drawCircle(x, y, radius, curColor);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void drawFilledCircle() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("x") && server.hasArg("y") && server.hasArg("radius")) {
+      int x, y, radius;
+      x = server.arg("x").toInt();
+      y = server.arg("y").toInt();
+      radius = server.arg("radius").toInt();
+      
+      display.fillCircle(x, y, radius, curColor);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void drawTriangle() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("x0") && server.hasArg("y0") && server.hasArg("x1") && server.hasArg("y1") && server.hasArg("x2") && server.hasArg("y2")) {
+      int x0, y0, x1, y1, x2, y2;
+      x0 = server.arg("x0").toInt();
+      y0 = server.arg("y0").toInt();
+      x1 = server.arg("x1").toInt();
+      y1 = server.arg("y1").toInt();
+      x2 = server.arg("x2").toInt();
+      y2 = server.arg("y2").toInt();
+      
+      display.drawTriangle(x0, y0, x1, y1, x2, y2, curColor);
+      server.send(200, "text/plain", "OK");
+    } else {
+      server.send(404, "text/plain", "404");
+    }
+  } else {
+    server.send(404, "text/plain", "404");
+  }
+}
+
+void drawFilledTriangle() {
+  if (server.method() == HTTP_POST) {
+    if (server.hasArg("x0") && server.hasArg("y0") && server.hasArg("x1") && server.hasArg("y1") && server.hasArg("x2") && server.hasArg("y2")) {
+      int x0, y0, x1, y1, x2, y2;
+      x0 = server.arg("x0").toInt();
+      y0 = server.arg("y0").toInt();
+      x1 = server.arg("x1").toInt();
+      y1 = server.arg("y1").toInt();
+      x2 = server.arg("x2").toInt();
+      y2 = server.arg("y2").toInt();
+      
+      display.fillTriangle(x0, y0, x1, y1, x2, y2, curColor);
       server.send(200, "text/plain", "OK");
     } else {
       server.send(404, "text/plain", "404");
--- wespdisp/wespdisp.py
+++ wespdisp/wespdisp.py
@@ -9,6 +9,7 @@ class wDisplay:
         self.ip = ip
         self.size = 0
 
+
     def setup(self, x:int=0, y:int=0, size:int=1):
         if self.debug: print(f"connecting to {self.ip}")
         req = requests.get(url=f"{self.url}")
@@ -19,6 +20,7 @@ class wDisplay:
         self.setSize(size)
         self.size = size
 
+
     def println(self, text:str, size:int=-1):
         if size > -1 and size != self.size:
             self.setSize(size)
@@ -26,6 +28,7 @@ class wDisplay:
         req = requests.post(url=f"{self.url}/println", data=data)
         if self.debug and req.status_code == 200:
             print(f"printed {text}")
+
 
     def print(self, text:str, size:int=-1):
         if size > -1 and size != self.size:
@@ -35,10 +38,18 @@ class wDisplay:
         if self.debug and req.status_code == 200:
             print(f"printed {text}")
 
+
     def clear(self):
         req = requests.post(url=f"{self.url}/clear")
         if self.debug and req.status_code == 200:
             print("cleared")
+
+    
+    def invert(self):
+        req = requests.post(url=f"{self.url}/invert")
+        if self.debug and req.status_code == 200:
+            print("inverted")
+
 
     def setCursor(self, x:int=0, y:int=0):
         data = { "x": str(x), "y": str(y) }
@@ -46,12 +57,14 @@ class wDisplay:
         if self.debug and req.status_code == 200:
             print(f"cursor set to {x},{y}")
     
+
     def setSize(self, size:int):
         data = { "size": str(size) }
         req = requests.post(url=f"{self.url}/setSize", data=data)
         self.size = size
         if self.debug and req.status_code == 200:
             print(f"font size set to {size}")
+
 
     def upload(self, text:str):
         pattern = r'\(?(\d+)\)?\[?(\d+)?;?(\d+)?\]?:(.+)'
@@ -65,12 +78,77 @@ class wDisplay:
             if size: self.setSize(size)
             if x and y: self.setCursor(x, y)
             self.println(tx)
+
+
+    def setColor(self, color:str="white"): # black or white
+        data = { "color": color }
+        req = requests.post(url=f"{self.url}/setColor", data=data)
+        if self.debug and req.status_code == 200:
+            print(f"color set to {color}")
     
-    def drawPixel(self, x:int=0, y:int=0):
+
+    def drawPixel(self, x:int, y:int):
         data = { "x": str(x), "y": str(y) }
         req = requests.post(url=f"{self.url}/drawPixel", data=data)
         if self.debug and req.status_code == 200:
             print(f"drawed pixel at {x},{y}")
+
+
+    def drawLine(self, x0:int, y0:int, x1:int, y1:int):
+        data = { "x0": str(x0), "y0": str(y0), "x1": str(x1), "y1": str(y1) }
+        req = requests.post(url=f"{self.url}/drawLine", data=data)
+        if self.debug and req.status_code == 200:
+            print(f"drawed line from {x0, y0} to {x1, y1}")
+
+
+    def drawRect(self, x:int, y:int, width:int, height:int):
+        data = { "x": str(x), "y": str(y), "width": str(width), "height": str(height) }
+        req = requests.post(url=f"{self.url}/drawRect", data=data)
+        if self.debug and req.status_code == 200:
+            print(f"drawed rect from {x, y}, width: {width}, height: {height}")
+
+
+    def drawFilledRect(self, x:int, y:int, width:int, height:int):
+        data = { "x": str(x), "y": str(y), "width": str(width), "height": str(height) }
+        req = requests.post(url=f"{self.url}/drawFilledRect", data=data)
+        if self.debug and req.status_code == 200:
+            print(f"drawed filled rect from {x, y}, width: {width}, height: {height}")
+
+
+    def drawRoundRect(self, x:int, y:int, width:int, height:int, radius:int):
+        data = { "x": str(x), "y": str(y), "width": str(width), "height": str(height), "radius": str(radius) }
+        req = requests.post(url=f"{self.url}/drawRoundRect", data=data)
+        if self.debug and req.status_code == 200:
+            print(f"drawed filled rect from {x, y}, width: {width}, height: {height}")
+
+
+    def drawFilledRoundRect(self, x:int, y:int, width:int, height:int, radius:int):
+        data = { "x": str(x), "y": str(y), "width": str(width), "height": str(height), "radius": str(radius) }
+        req = requests.post(url=f"{self.url}/drawFilledRoundRect", data=data)
+        if self.debug and req.status_code == 200:
+            print(f"drawed filled round rect from {x, y}, width: {width}, height: {height}")
+
+
+    def drawCircle(self, x:int, y:int, radius:int):
+        data = { "x": str(x), "y": str(y), "radius": str(radius) }
+        req = requests.post(url=f"{self.url}/drawCircle", data=data)
+        if self.debug and req.status_code == 200:
+            print(f"drawed circle in {x, y}, with radius {radius}")
+
+
+    def drawCircle(self, x:int, y:int, radius:int):
+        data = { "x": str(x), "y": str(y), "radius": str(radius) }
+        req = requests.post(url=f"{self.url}/drawFilledCircle", data=data)
+        if self.debug and req.status_code == 200:
+            print(f"drawed filled circle in {x, y}, with radius {radius}")
+
+
+    def drawTriangle(self, x0:int, y0:int, x1:int, y1:int, x2:int, y2:int):
+        data = { "x0": str(x0), "y0": str(y0), "x1": str(x1), "y1": str(y1), "x2": str(x2), "y2": str(y2) }
+        req = requests.post(url=f"{self.url}/drawTriangle", data=data)
+        if self.debug and req.status_code == 200:
+            print(f"drawed triangle with vertexes {x0, y0}, {x1, y1}, {x2, y2}")
+
 
     def update(self):
         req = requests.post(url=f"{self.url}/update")
